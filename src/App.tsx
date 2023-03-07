8deafd0554b1d54ac342975e23848c587f65d1c0
Pedro <pedroaugustosilva.880>
2023-03-07 05:36:27 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="text-white">
+      <h1 className="text-white">Jogo de Matematica</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
