0f71cea1ae5c9b5bd945c78effde0dfbe6f19d96
Pedro <pedroaugustosilva.880>
2023-03-30 11:28:16 -0300

    modify-modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { changeAvatar, avatar } from "../utils/avatars";
+import { changeAvatar, avatar } from "../services/utils/avatars";
 import { useForm, SubmitHandler } from "react-hook-form";
-import usePersistedState from "../utils/usePersistedState";
+import usePersistedState from "../hooks/usePersistedState";
 import * as Dialog from "@radix-ui/react-dialog";
 import { useState } from "react";
 import Modal from "../components/Modal";
@@ -17,10 +17,37 @@ interface IUserForm {
   username: string;
 }
 
-const Home = () => {
-  const { register, handleSubmit } = useForm<IUser>();
+interface IRoom {
+  room_id: string;
+  room_name: string;
+  participants: [
+    {
+      name: string;
+      socket_id: string;
+    }
+  ];
+}
 
-  const [image, setImage] = useState<string>();
+interface IError {
+  message: string;
+  error: boolean;
+}
+
+const Home = () => {
+  const io = socket("http://localhost:3000/");
+
+  const [isConnected, setConnected] = useState<boolean>(false);
+  const [oponent, setOponent] = useState<string>();
+  const [room, setRoom] = useState<IRoom>();
+  const [error, setError] = useState<IError>();
+
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  io.on("connect", () => {
+    setConnected(true);
+  });
+
+  const { register, handleSubmit } = useForm<IUser>();
 
   const [user, setUser] = usePersistedState<IUser>("user", {
     username: "",
@@ -51,64 +78,94 @@ const Home = () => {
     }
   };
 
-  const handleSubmitForm: SubmitHandler<IUserForm> = (data) => {
+  const handleSubmitForm: SubmitHandler<IUserForm> = async (data) => {
     setUser((preValue) => {
       return {
         username: data.username,
         avatar: preValue.avatar,
       };
     });
-    const io = socket("http://localhost:3000/");
-    io.on("hello", (socket) => {
-      console.log(socket.id);
+
+    io.on("already-exist-user-message", (data3) => {
+      setError(data3);
+      console.log(data3);
+    });
+    if (!error?.error) {
+      io.on("loading-room", (data2) => {
+        console.log(data2);
+        setOpen(true);
+      });
+    }
+    io.emit("find-room", {
+      ...user,
+      socket_id: io.id,
+      room_name: "room" + Math.random().toString(8).substr(2),
     });
   };
-
   return (
-    <div className="w-content h-content bg-white drop-shadow-2xl rounded-2xl py-10 px-5">
-      <div className="flex items-center flex-col justify-center w-full">
-        <div className=" h-content flex flexl-row items-center group ">
-          <button
-            className="w-content opacity-0 group-hover:opacity-100 transition-all"
-            onClick={() => handleChangeImage("back")}
-          >
-            <CaretLeft size={40} color={colors.blue[500]} />
-          </button>
-          <img
-            src={user.avatar}
-            alt=""
-            className="rounded-full border-4 border-blue-500 w-52 h-52 p-1"
-          />
-          <button
-            className="w-content opacity-0 group-hover:opacity-100 transition-all"
-            onClick={() => handleChangeImage("skip")}
-          >
-            <CaretRight size={40} color={colors.blue[500]} />
-          </button>
+    <>
+      {!isConnected && (
+        <div className="w-screen h-screen absolute inset-0 bg-black bg-opacity-30 z-10 flex items-center justify-center">
+          <div className="flex items-center justify-center p-5 bg-black bg-opacity-60 rounded-xl">
+            <div className="animate-ping w-7 h-7 rounded-full bg-white" />
+          </div>
         </div>
-        <form
-          className="flex flex-col items-center"
-          onSubmit={handleSubmit(handleSubmitForm)}
-        >
-          <input
-            type="text"
-            className="border-2 border-blue-500 rounded-full h-10 w-full px-3 outline-none mt-5"
-            placeholder="Nome de usuário"
-            defaultValue={user.username}
-            {...register("username")}
-          />
-          <Dialog.Root>
-            <Dialog.Trigger
-              className="h-10 w-full mt-3 bg-blue-500 rounded-full px-3 text-lg text-white font-semibold transition-all hover:scale-105"
+      )}
+
+      <div className="w-content h-content bg-white drop-shadow-2xl rounded-2xl py-10 px-5 ">
+        <div className="flex items-center flex-col justify-center w-full">
+          <div className=" h-content flex flexl-row items-center group">
+            <button
+              className="w-content opacity-0 group-hover:opacity-100 transition-all"
+              onClick={() => handleChangeImage("back")}
+            >
+              <CaretLeft size={40} color={colors.blue[500]} />
+            </button>
+            <img
+              src={user.avatar}
+              alt=""
+              className="rounded-full border-4 border-blue-500 w-52 h-52 p-1"
+            />
+            <button
+              className="w-content opacity-0 group-hover:opacity-100 transition-all"
+              onClick={() => handleChangeImage("skip")}
+            >
+              <CaretRight size={40} color={colors.blue[500]} />
+            </button>
+          </div>
+          <form
+            className="flex flex-col items-center"
+            onSubmit={handleSubmit(handleSubmitForm)}
+          >
+            <input
+              type="text"
+              className="border-2 border-blue-500 rounded-full h-10 w-full px-3 outline-none mt-5"
+              placeholder="Nome de usuário"
+              defaultValue={user.username}
+              {...register("username")}
+            />
+            <span className="text-red-500 mt-1">
+              {error?.error ? error?.message : ""}
+            </span>
+            <button
+              className="h-10 w-full mt-1 bg-blue-500 rounded-full px-3 text-lg text-white font-semibold transition-all hover:scale-105"
               type="submit"
             >
               Procurar oponente
-            </Dialog.Trigger>
-            <Modal avatar={user.avatar} username={user.username} />
-          </Dialog.Root>
-        </form>
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+      {isOpen && (
+        <Modal
+          avatar={user.avatar}
+          username={user.username}
+          oponent={oponent}
+          room_id={room?.room_id}
+          socket={io}
+        />
+      )}
+    </>
   );
 };
 
